18609b427153f900dea4145ad175c54996036aa3
VicVEVO <viclouisnoe@gmail.com>
2025-07-08 16:53:03 +0200

    day 12: Continue implementation Chi2

--- programs/src/constants.py
+++ programs/src/constants.py
@@ -11,20 +11,22 @@ GAMMA = 0.55 # γ
 
 C = 299792458
 
+### RSD
 data_rsd = pd.read_csv("../data/fsigma8_data.dat", sep=';', header=0)
-data_pantheon = pd.read_csv("../data/DataRelease/Pantheon+_Data/4_DISTANCES_AND_COVAR/Pantheon+SH0ES.dat", sep=' ', header=0)
-cov_mat = pd.read_csv("../data/DataRelease/Pantheon+_Data/4_DISTANCES_AND_COVAR/Pantheon+SH0ES_STAT+SYS.cov")
-
-
-z_data_panth = data_pantheon['zCMB']
-m_b_corr_panth = data_pantheon('m_b_corr')
-mu_data_panth = data_pantheon['MU_SH0ES']
-ceph_dist_panth = data_pantheon('CEPH_DIST')
-is_calibrator_panth = data_pantheon('IS_CALIBRATOR')
-
 
 z_data = data_rsd['z']
 fs8_data = data_rsd['fsig8']
 fs8_err_plus = data_rsd['fsig8_err_plus']
 fs8_err_minus = data_rsd['fsig8_err_minus']
 fs8_err = [fs8_err_plus, fs8_err_minus]
+
+### Pantheon+
+# data_pantheon = pd.read_csv("../data/DataRelease/Pantheon+_Data/4_DISTANCES_AND_COVAR/Pantheon+SH0ES.dat", sep=' ', header=0)
+# cov_mat = pd.read_csv("../data/DataRelease/Pantheon+_Data/4_DISTANCES_AND_COVAR/Pantheon+SH0ES_STAT+SYS.cov")
+
+
+# z_data_panth = data_pantheon['zCMB']
+# m_b_corr_panth = data_pantheon('m_b_corr')
+# mu_data_panth = data_pantheon['MU_SH0ES']
+# ceph_dist_panth = data_pantheon('CEPH_DIST')
+# is_calibrator_panth = data_pantheon('IS_CALIBRATOR')
